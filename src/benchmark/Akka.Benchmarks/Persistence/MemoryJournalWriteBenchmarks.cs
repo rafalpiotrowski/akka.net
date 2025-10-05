a08bf8bc8561622ef558bd6f4f41f7df34908d14
Aaron Stannard <aaron@petabridge.com>
2025-10-05 13:17:12 -0500

    Refactor memory journal benchmarks into separate classes (#7878)
    * Refactor memory journal benchmarks into separate classes
    
    Split MemoryJournalBenchmarks into two specialized benchmark classes:
    
    - MemoryJournalWriteBenchmarks: Measures pure write performance for
      standard and tagged events without recovery overhead
    - MemoryJournalRecoveryBenchmarks: Measures recovery performance with
      pre-populated data in IterationSetup to avoid conflating write and
      recovery costs
    
    This separation provides more accurate measurements by ensuring each
    benchmark measures exactly one operation type.
    
    * Add missing using directive for Tagged type
    
    * Fix IterationSetup methods to be synchronous for BenchmarkDotNet compatibility

--- src/benchmark/Akka.Benchmarks/Persistence/MemoryJournalWriteBenchmarks.cs
+++ src/benchmark/Akka.Benchmarks/Persistence/MemoryJournalWriteBenchmarks.cs
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------
-// <copyright file="MemoryJournalBenchmarks.cs" company="Akka.NET Project">
+// <copyright file="MemoryJournalWriteBenchmarks.cs" company="Akka.NET Project">
 //     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2025 .NET Foundation <https://github.com/akkadotnet/akka.net>
 // </copyright>
@@ -18,7 +18,7 @@ using static Akka.Benchmarks.Configurations.BenchmarkCategories;
 namespace Akka.Benchmarks.Persistence
 {
     [Config(typeof(MicroBenchmarkConfig))]
-    public class MemoryJournalBenchmarks
+    public class MemoryJournalWriteBenchmarks
     {
         private static readonly Config Config = ConfigurationFactory.ParseString(@"
             akka.persistence.journal.plugin = ""akka.persistence.journal.inmem""
@@ -67,40 +67,12 @@ namespace Akka.Benchmarks.Persistence
 
         [Benchmark]
         [BenchmarkCategory(MicroBenchmark)]
-        public async Task Write_and_replay_events()
+        public async Task Write_tagged_events_to_memory_journal()
         {
-            // Write events
-            for (var i = 0; i < EventCount; i++)
-            {
-                await _persistentActor.Ask<string>($"event-{i}", TimeSpan.FromSeconds(10));
-            }
-
-            // Trigger recovery by stopping and restarting
-            var persistenceId = await _persistentActor.Ask<string>("get-id", TimeSpan.FromSeconds(5));
-            await _persistentActor.GracefulStop(TimeSpan.FromSeconds(5));
-
-            _persistentActor = _system.ActorOf(Props.Create(() => new BenchmarkPersistentActor(persistenceId)));
-
-            // Wait for recovery to complete
-            await _persistentActor.Ask<int>("get-count", TimeSpan.FromSeconds(10));
-        }
-
-        [Benchmark]
-        [BenchmarkCategory(MicroBenchmark)]
-        public async Task Write_tagged_events_and_query()
-        {
-            // Write tagged events
             for (var i = 0; i < EventCount; i++)
             {
                 await _persistentActor.Ask<string>($"tagged-event-{i}", TimeSpan.FromSeconds(10));
             }
-
-            // Query by tag (simulated via replay)
-            var persistenceId = await _persistentActor.Ask<string>("get-id", TimeSpan.FromSeconds(5));
-            await _persistentActor.GracefulStop(TimeSpan.FromSeconds(5));
-
-            _persistentActor = _system.ActorOf(Props.Create(() => new BenchmarkPersistentActor(persistenceId)));
-            await _persistentActor.Ask<int>("get-count", TimeSpan.FromSeconds(10));
         }
 
         #region Test Actor
